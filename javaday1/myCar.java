78840ba9f5cda778b9dca1088592213606ee865f
Pavitha <89039569+athivap@users.noreply.github.com>
2022-05-10 23:05:36 +0530

    Add files via upload

---
+++ javaday1/myCar.java
@@ -0,0 +1,13 @@
+package javaday1;
+
+public class myCar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Car c =new Car();
+c.applyBreak();
+c.applyAccelerate();
+c.applyGear();
+c.switchonAC();
+}
+}
